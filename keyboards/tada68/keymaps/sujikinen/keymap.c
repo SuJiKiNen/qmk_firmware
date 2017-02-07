a39cc1effb5df89f0d3327b323094dac57bce979
luishen <luishen.@vip.qq.com>
2017-02-07 14:40:27 +0800

    finish hold for shift,tap for oneshot symbol layer

--- keyboards/tada68/keymaps/sujikinen/keymap.c
+++ keyboards/tada68/keymaps/sujikinen/keymap.c
@@ -1,9 +1,5 @@
 #include "tada68.h"
 
-
-// Used for SHIFT_ESC
-#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -32,8 +28,9 @@ enum macros{
 #define M_LSFT              M(MACRO_SYMBOL_LSFT_TAP)
 #define M_RSFT              M(MACRO_SYMBOL_RSFT_TAP)
 #define _______ KC_TRNS
-#define DU_ENT CTL_T(KC_ENT)
-#define DU_TAB ALL_T(KC_TAB)
+#define DU_ENT  CTL_T(KC_ENT)
+#define DU_TAB  ALT_T(KC_TAB)
+#define DU_BSLS ALT_T(KC_BSLS)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap QW: (Base Layer) Default Layer
      * ,----------------------------------------------------------------.
@@ -50,14 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [QW] = KEYMAP_ANSI(
             KC_ESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
-            DU_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
+            DU_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,DU_BSLS,KC_DEL, \
             TD(TG_LY), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         DU_ENT ,KC_PGUP,\
             M_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         M_RSFT ,KC_UP  ,KC_PGDN,\
             KC_LCTL,   KC_LGUI,KC_LALT,                KC_SPC,                         KC_RALT,MO(FN), KC_RCTRL,KC_LEFT,KC_DOWN,KC_RGHT),
 
     [DV] = KEYMAP_ANSI(
             KC_ESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_LBRC, KC_RBRC,KC_BSPC,KC_GRV, \
-            DU_TAB,    KC_QUOT,KC_COMM,KC_DOT, KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_SLSH, KC_EQL ,KC_BSLS,KC_DEL, \
+            DU_TAB,    KC_QUOT,KC_COMM,KC_DOT, KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_SLSH, KC_EQL ,DU_BSLS,KC_DEL, \
             TD(TG_LY), KC_A,   KC_O,   KC_E,   KC_U,   KC_I,   KC_D,   KC_H,   KC_T,   KC_N,   KC_S,   KC_MINS,         DU_ENT ,KC_PGUP,\
             M_LSFT,    KC_SCLN,KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,            M_RSFT,KC_UP  ,KC_PGDN,\
             KC_LCTL,   KC_LGUI,KC_LALT,                KC_SPC,                         KC_RALT,MO(FN), KC_RCTRL,KC_LEFT,KC_DOWN,KC_RGHT),
@@ -78,19 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-//static bool CTRL_PRESSED = false;
 void dance_layer_switch(qk_tap_dance_state_t *state, void *user_data) {
     switch (state->count) {
         case 1:
-            /*
-            if (timer_elapsed(state->timer) > 200) {
-                clear_oneshot_mods();
-                register_code16(KC_LCTL);
-                CTRL_PRESSED = true;
-            }else{
-                set_oneshot_mods(MOD_LSFT);
-            }
-            */
             register_code16(KC_LCTL);
             break;
         case 2:
@@ -98,17 +85,11 @@ void dance_layer_switch(qk_tap_dance_state_t *state, void *user_data) {
                 layer_move(DV);
             }else{
                 layer_move(QW);
-            }		
+            }
             break;
     }
 }
 void dance_layer_reset(qk_tap_dance_state_t *state,void *user_data){
-    /*
-    if( CTRL_PRESSED ) {
-        unregister_code16(KC_LCTL);
-        CTRL_PRESSED = false;
-    }
-    */
     unregister_code16(KC_LCTL);
 }
 qk_tap_dance_action_t tap_dance_actions[] = {
@@ -135,12 +116,27 @@ void matrix_scan_user(void){
 void matrix_init_user(void){
     led_set_dv_indicator();
 }
-//static bool shift_interrupted = false;
+static bool lshift_interrupted = false;
+static bool rshift_interrupted = false;
 static uint16_t lsft_pressed_time = 0;
 static uint16_t rsft_pressed_time = 0;
+
+// if we pressed shift,before we release the shift key,
+// if we pressed any other key,not trigger the oneshot layer
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed){
+        if (lsft_pressed_time > 0 ){
+            lshift_interrupted = true;
+        }
+        if (rsft_pressed_time > 0 ){
+            rshift_interrupted = true;
+        }
+    }
+    return true;
+}
+
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-    //uint8_t tap_count = record->tap.count;
     switch(id){
         case MACRO_BREATH_TOGGLE:
             if (record->event.pressed)
@@ -169,17 +165,19 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
                 breathing_defaults();
             }
             break;
+        // hold for shift,tap for one shot symbol layer
         case MACRO_SYMBOL_LSFT_TAP:
             if (record->event.pressed) {
                 register_code16(KC_LSFT);
                 lsft_pressed_time = timer_read();
             }else{
                 unregister_code16(KC_LSFT);
-                if (!record->tap.interrupted && timer_elapsed(lsft_pressed_time) < 200 ){
+                if (!lshift_interrupted && timer_elapsed(lsft_pressed_time) < 200 ){
                     set_oneshot_layer(SY, ONESHOT_START);
                     clear_oneshot_layer_state(ONESHOT_PRESSED);
                 }
                 lsft_pressed_time = 0;
+                lshift_interrupted = false;
             }
             break;
 
@@ -189,66 +187,14 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
                 rsft_pressed_time = timer_read();
             }else{
                 unregister_code16(KC_RSFT);
-                if (!record->tap.interrupted && timer_elapsed(rsft_pressed_time) < 200 ){
+                if (!rshift_interrupted && timer_elapsed(rsft_pressed_time) < 200 ){
                     set_oneshot_layer(SY, ONESHOT_START);
                     clear_oneshot_layer_state(ONESHOT_PRESSED);
                 }
                 rsft_pressed_time = 0;
+                rshift_interrupted = false;
             }
-            /*
-            if (record->event.pressed)
-            {
-                if (tap_count > 0 ) {
-                    layer_on( SY );
-                }else{
-                    register_code16(KC_LSFT);
-                }
-            }else{
-                if (tap_count > 0 ) {
-                    if (record->tap.interrupted) {
-                                record->tap.count = 0;
-                                layer_off( SY );
-                    }
-                }else{
-                    unregister_code16(KC_LSFT);
-                }
-            }
-            */
             break;
     }
     return MACRO_NONE;
 }
-
-/*enum function_id {
-//SHIFT_ESC,
-};
-
-const uint16_t PROGMEM fn_actions[] = {
-//[0]  = ACTION_FUNCTION(SHIFT_ESC),
-};
-
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-static uint8_t shift_esc_shift_mask;
-switch (id) {
-case SHIFT_ESC:
-shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
-if (record->event.pressed) {
-if (shift_esc_shift_mask) {
-add_key(KC_GRV);
-send_keyboard_report();
-} else {
-add_key(KC_ESC);
-send_keyboard_report();
-}
-} else {
-if (shift_esc_shift_mask) {
-del_key(KC_GRV);
-send_keyboard_report();
-} else {
-del_key(KC_ESC);
-send_keyboard_report();
-}
-}
-break;
-}
-}*/
