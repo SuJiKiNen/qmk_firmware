2f3cf25861a433f89d94ee9e6a5a85e4c53420bf
SuJiKiNen <luishen@vip.qq.com>
2017-04-28 09:15:12 -0700

    add dual space

--- keyboards/tada68/keymaps/sujikinen/keymap.c
+++ keyboards/tada68/keymaps/sujikinen/keymap.c
@@ -28,6 +28,7 @@ enum macros{
 #define M_LSFT              M(MACRO_SYMBOL_LSFT_TAP)
 #define M_RSFT              M(MACRO_SYMBOL_RSFT_TAP)
 #define _______ KC_TRNS
+#define DU_SPC  LT(SY,KC_SPC)
 #define DU_ENT  CTL_T(KC_ENT)
 #define DU_TAB  ALT_T(KC_TAB)
 #define DU_BSLS ALT_T(KC_BSLS)
@@ -50,14 +51,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             DU_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,DU_BSLS,KC_DEL, \
             TD(TG_LY), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         DU_ENT ,KC_PGUP,\
             M_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         M_RSFT ,KC_UP  ,KC_PGDN,\
-            KC_LCTL,   KC_LGUI,KC_LALT,                KC_SPC,                         KC_RALT,MO(FN), KC_RCTRL,KC_LEFT,KC_DOWN,KC_RGHT),
+            KC_LCTL,   KC_LGUI,KC_LALT,                DU_SPC,                         KC_RALT,MO(FN), KC_RCTRL,KC_LEFT,KC_DOWN,KC_RGHT),
 
     [DV] = KEYMAP_ANSI(
             KC_ESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_LBRC, KC_RBRC,KC_BSPC,KC_GRV, \
             DU_TAB,    KC_QUOT,KC_COMM,KC_DOT, KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_SLSH, KC_EQL ,DU_BSLS,KC_DEL, \
             TD(TG_LY), KC_A,   KC_O,   KC_E,   KC_U,   KC_I,   KC_D,   KC_H,   KC_T,   KC_N,   KC_S,   KC_MINS,         DU_ENT ,KC_PGUP,\
             M_LSFT,    KC_SCLN,KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,            M_RSFT,KC_UP  ,KC_PGDN,\
-            KC_LCTL,   KC_LGUI,KC_LALT,                KC_SPC,                         KC_RALT,MO(FN), KC_RCTRL,KC_LEFT,KC_DOWN,KC_RGHT),
+            KC_LCTL,   KC_LGUI,KC_LALT,                DU_SPC,                         KC_RALT,MO(FN), KC_RCTRL,KC_LEFT,KC_DOWN,KC_RGHT),
 
 
     [FN] = KEYMAP_ANSI(
@@ -68,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______,_______,_______,                _______,                        _______,_______,_______,_______,M_BSPDD,_______),
     [SY] = KEYMAP_ANSI(
             _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,KC_F11,  KC_F12,_______,_______,\
-            _______,_______,_______,_______,KC_LPRN,_______,_______,KC_RPRN,_______,_______,_______,_______,_______,_______,_______,\
-            _______,_______,_______,_______,KC_LCBR,_______,_______,KC_RCBR,_______,_______,_______,_______,        _______,_______,\
+            _______,_______,  KC_UP,_______,KC_LPRN,_______,_______,KC_RPRN,_______,_______,_______,_______,_______,_______,_______,\
+            KC_BSPC,KC_LEFT,KC_DOWN,KC_RGHT,KC_LCBR,_______,_______,KC_RCBR,_______,_______,_______,_______,        _______,_______,\
             _______,_______,_______,_______,KC_LBRC,_______,_______,KC_RBRC,_______,_______,_______,        _______,_______,_______,\
             _______,_______,_______,                _______,                        _______,_______,_______,_______,_______,_______),
 };
