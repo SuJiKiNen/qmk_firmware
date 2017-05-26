cd1d026dd69ab48538310947208a4120e74e5c9e
luishen <luishen@vip.qq.com>
2017-05-26 14:40:47 +0800

    fix xd60 dual space

--- keyboards/xd60/keymaps/sujikinen/keymap.c
+++ keyboards/xd60/keymaps/sujikinen/keymap.c
@@ -29,6 +29,7 @@ enum macros{
 #define DU_ENT  CTL_T(KC_ENT)
 #define DU_TAB  ALT_T(KC_TAB)
 #define DU_BSLS ALT_T(KC_BSLS)
+#define DU_SPC  LT(SY,KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -37,15 +38,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_LBRC,KC_RBRC, KC_GRV, KC_BSPC,\
              DU_TAB,KC_QUOT,KC_COMM, KC_DOT,   KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,KC_SLSH, KC_EQL,         DU_BSLS,\
              TD(LS),   KC_A,   KC_O,   KC_E,   KC_U,   KC_I,   KC_D,   KC_H,   KC_T,   KC_N,   KC_S,KC_MINS,  KC_NO,          DU_ENT,\
-             M_LSFT,  KC_NO,KC_SCLN,   KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V,   KC_Z, M_RSFT, KC_DEL,   KC_UP,\
-            KC_LCTL,KC_LGUI,KC_LALT,                 KC_SPC,                                KC_RALT, MO(FN),KC_LEFT,KC_DOWN,KC_RIGHT),
+            KC_LSFT,  KC_NO,KC_SCLN,   KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,KC_RSFT, KC_DEL,   KC_UP,\
+            KC_LCTL,KC_LGUI,KC_LALT,                 DU_SPC,                                KC_RALT, MO(FN),KC_LEFT,KC_DOWN,KC_RIGHT),
     // 1: QWERTY Layer
     [QW] = KEYMAP(
              KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS, KC_EQL, KC_GRV, KC_BSPC,\
              DU_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC,         DU_BSLS,\
              TD(LS),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,  KC_NO,          DU_ENT,\
-             M_LSFT,  KC_NO,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,M_RSFT , KC_DEL,   KC_UP,\
-            KC_LCTL,KC_LGUI,KC_LALT,                 KC_SPC,                                KC_RALT, MO(FN),KC_LEFT,KC_DOWN,KC_RIGHT),
+            KC_LSFT,  KC_NO,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT, KC_DEL,   KC_UP,\
+            KC_LCTL,KC_LGUI,KC_LALT,                 DU_SPC,                                KC_RALT, MO(FN),KC_LEFT,KC_DOWN,KC_RIGHT),
 
 
     [FN] = KEYMAP(
@@ -56,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______,_______,_______,                _______,                                _______,_______,_______,_______,M_BSPDD),
     [SY] = KEYMAP(
             _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,KC_F11,  KC_F12,_______,_______,\
-            _______,_______,_______,_______,KC_LPRN,_______,_______,KC_RPRN,_______,_______,_______,_______,_______,        _______,\
-            _______,_______,_______,_______,KC_LCBR,_______,_______,KC_RCBR,_______,_______,_______,_______,_______,        _______,\
+            _______,_______,  KC_UP,_______,KC_LPRN,_______,_______,KC_RPRN,_______,_______,_______,_______,_______,        _______,\
+            _______,KC_LEFT,KC_DOWN,KC_RGHT,KC_LCBR,_______,_______,KC_RCBR,_______,_______,_______,_______,_______,        _______,\
             _______,_______,_______,_______,_______,KC_LBRC,_______,_______,KC_RBRC,_______,_______,_______,_______,_______,_______,\
             _______,_______,_______,                _______,                                _______,_______,_______,_______,_______),
 
@@ -102,7 +103,7 @@ void matrix_scan_user(void){
 void matrix_init_user(void){
     xd60_caps_led_on();
 }
-
+/*
 static bool lshift_interrupted = false;
 static bool rshift_interrupted = false;
 static uint16_t lsft_pressed_time = 0;
@@ -189,3 +190,4 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     }
     return MACRO_NONE;
 }
+*/
